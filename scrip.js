4d9489cab0ab4e4bfb5c9dad51d095264a4470fd
hooonzaa <145441106+hooonzaa@users.noreply.github.com>
2023-10-30 12:21:48 +0100

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,56 @@
+function Pythagorova() {
+
+    const numberA = parseFloat(document.getElementById('a').value)
+    const numberB = parseFloat(document.getElementById('b').value)
+
+    let.result = numberA
+
+}
+if (!isNaN) (a) && (!isNaN)(b)
+
+
+
+Math.sqrt('c')
+
+
+if (true) {
+    console.log('Ahoj')
+}
+
+function program() {
+    if (!10) (10)
+    Let.gettresoult('F')
+
+
+
+}
+function Ověření() {
+    const inputAge = parseFloat(document.getElementById('18×').value)
+    if (inputAge >= 18) {
+        console.log('Dospělí uživatel')
+}
+    else if (inputAge < 18){
+            console.log('Smol')
+}
+}
+function Rokchecker() {
+
+    const inputRok = parseFloat(document.getElementById('Rok').value) 
+    if (inputRok < 2005) {
+        document.getElementById('out').value = "Dospělí"
+        
+}
+    else if (inputRok >= 2005){
+        document.getElementById('out').value = "dite"
+          
+}
+
+}
+function checker() {
+
+    let inputRok2 = parseFloat(document.getElementById('Rok2').value) 
+    
+        inputRok2 =  inputRok2 + 10
+        document.getElementById('OP2').innerHTML = inputRok2
+
+}
